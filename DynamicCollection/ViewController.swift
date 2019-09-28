93ed32ea3c55bdb1320e75cf5826e3fd3184356f
Mohamed Ramadan <mohamed_r_2015@yahoo.com>
2019-09-28 12:19:45 +0200

    Initial Commit

---
+++ DynamicCollection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicCollection
+//
+//  Created by macbook on 9/28/19.
+//  Copyright © 2019 macbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
