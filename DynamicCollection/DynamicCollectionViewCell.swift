38337f01aec39d6fca7005ec29a9752aabc1fb56
Mohamed Ramadan <mohamed_r_2015@yahoo.com>
2019-09-28 12:43:36 +0200

    first commit

---
+++ DynamicCollection/DynamicCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DynamicCollectionViewCell.swift
+//  DynamicCollection
+//
+//  Created by macbook on 9/28/19.
+//  Copyright © 2019 macbook. All rights reserved.
+//
+
+import UIKit
+
+class DynamicCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
